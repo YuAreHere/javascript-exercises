b0ab984c9a802047417eae74834a771cd9acc1dc
YuAreHere <verballyrebellious@gmail.com>
2022-07-16 22:50:13 +0100

    Complete 03 Exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+function reverseString(reversed) {
+    //reversedString = "";
+    let splitString = reversed.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    return joinArray;
 };
 
 // Do not edit below this line
