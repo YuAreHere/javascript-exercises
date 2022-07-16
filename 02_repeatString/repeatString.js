e74313161c2c97171af0c1511ff64dd56bc98d55
YuAreHere <verballyrebellious@gmail.com>
2022-07-16 21:59:48 +0100

    Complete 02 Lessons

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(repeater, num) {
 
+let repeatedString = "";
+
+if (num < 0) {
+    return "ERROR";
+}
+
+while (num > 0) {
+
+repeatedString += repeater;
+num--;
+}
+return repeatedString;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
