2873e338e23d4418d91976b67b25eec34323e5d3
YuAreHere <verballyrebellious@gmail.com>
2022-07-17 21:36:19 +0100

    Complete 04 Exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, num1) {
+    const args = Array.from(arguments).slice(1);
+        //if (args.length == array.length) {
+          //  throw Error ("Can't remove all elements");
+        //}
+        let filteredArray = array.filter((elem) => {
+            return !args.includes(elem)
+        })
+        return filteredArray;
+    //let index = array.indexOf(num1);
+    //let removeSingleValue = array.splice(index, 1);
+      // return removeSingleValue;
 
 };
 
