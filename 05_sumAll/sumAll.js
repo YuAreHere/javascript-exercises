63365f892023b57749d1f629948a4210d413d7cf
YuAreHere <verballyrebellious@gmail.com>
2022-07-18 12:15:26 +0100

    Complete 05 Exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+function sumAll(min, max) {
+    let maxi = Math.max(min, max);
+    let mini = Math.min(min, max);
+    if (min < 0 || max < 0) {
+        return "ERROR";
+    }
 
-};
+    if (typeof min === "string" || typeof max === "string") {
+        return "ERROR";
+    }
+
+    if (Array.isArray(min) || Array.isArray(max)) {
+        return "ERROR";
+    }
+
+    return ((maxi-mini)+1) * (mini + maxi) / 2;
+    
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
